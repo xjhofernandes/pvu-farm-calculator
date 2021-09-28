e312a4d30d6de0f2f9bf76dfbf37157a709e16db
Jonathan Fernandes <46504678+xjhofernandes@users.noreply.github.com>
2021-09-28 12:49:43 -0300

    :pencil: revertend oajuste

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
-chainWebpack: config => {
-  const svgRule = config.module.rule('svg')
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
 
-  svgRule.uses.clear()
+    svgRule.uses.clear();
 
-  /* svgRule.use('vue-svg-loader').loader('vue-svg-loader') */
-  svgRule
-    .use('babel-loader')
-    .loader('babel-loader')
-    .end()
-    .use('vue-svg-loader')
-    .loader('vue-svg-loader')
-}
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader-v16') // or `vue-loader-v16` if you are using a preview support of Vue 3 in Vue CLI
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+};
